004842cacdd0f6ff97c387e7f9c0e5780e197bfd
Kurt Kerns <kurtkerns20@gmail.com>
2022-02-04 16:19:41 -0600

    runs through, need to debug card display

--- twenty_one.rb
+++ twenty_one.rb
@@ -1,5 +1,10 @@
+# twenty_one.rb
+
+require 'pry-byebug'
+
 WINNING_VALUE = 21
 FORCE_DEALER_STAY = 17
+
 SUITS = %w(S H D C)
 NUMBERED_RANKS = (2..10).map(&:to_s)
 ROYAL_RANKS = %w(J Q K)
@@ -13,6 +18,7 @@ SUIT_SYMBOLS = {
   '*' => "*"
 }
 SLEEP_DURATION = 1.5
+
 MESSAGES = {
   welcome: "Welcome to #{WINNING_VALUE}!",
   rules: <<~MSG,
@@ -25,15 +31,16 @@ MESSAGES = {
     - The greater hand value wins if neither player busts.
   MSG
   thank_you: "Thank you for playing #{WINNING_VALUE}!",
-  quote: "Don't cry because it's over, smile because it happened. - Dr. Seuss"
+  quote: "Don't cry because it's over, smile because it happened. \n
+    - Dr. Seuss",
   continue: "Press Enter to continue.",
   title: "You are playing #{WINNING_VALUE}.",
   player_turn: "It's your turn.",
-  players_hand: "Your hand:",
+  players_hand: "Your Hand:",
   player_action: "Hit or Stay? (h/hit or s/stay)",
   player_busted: "You busted!",
   player_won: "You won!",
-  dealer_hand: "Dealer's Hand:"
+  dealer_hand: "Dealer's Hand:",
   dealer_turn: "It's the dealer's turn.",
   dealer_stay: "Dealer chose to stay.",
   dealer_hit: "Dealer chose to hit.",
@@ -43,9 +50,30 @@ MESSAGES = {
   game_over: "Game over.",
   play_again: "Want to play again? (y/yes or n/no)",
   invalid_input: "Please enter a valid input.",
-  separator_line: '-' * 80
+  separator_line: '-' * 50
 }
 YES_OR_NO = %w(y n yes no)
+
+
+def play_twenty_one
+  display_welcome
+  loop do
+    play_match
+    break unless play_again?
+  end
+  display_goodbye
+end
+
+def play_match
+  cards = initialize_cards
+  totals = initialize_totals
+  # binding.pry
+  deal_cards(cards, totals)
+  display_hands(cards, totals, hide_dealer_card: true)
+  player_turn(cards, totals)
+  dealer_turn(cards, totals) unless bust?(totals[:player])
+  display_end_of_game(totals)
+end
 
 def initialize_cards
   {
@@ -58,7 +86,7 @@ end
 def initialize_totals
   {
     player: 0,
-    computer: 0,
+    dealer: 0,
   }
 end
 
@@ -66,7 +94,50 @@ def initialize_deck
   SUITS.product(RANKS).shuffle
 end
 
-def deal_cards(cards)
+# Player Action
+def player_turn(cards, totals)
+  prompt MESSAGES[:player_turn]
+  action = nil
+  loop do
+    action = get_player_action
+    deal_card!(:player, cards, totals) if action == :hit
+    display_hands(cards, totals, hide_dealer_card: true)
+    break if action == :stay || bust?(totals[:player])
+  end
+end
+
+def get_player_action
+  answer = ''
+  loop do
+    prompt MESSAGES[:player_action]
+    answer = gets.chomp.downcase
+    break if %w(h hit s stay).include?(answer)
+    prompt MESSAGES[:invalid_input]
+  end
+
+  answer.start_with?('h') ? :hit : :stay
+end
+
+# Dealer Action
+def dealer_turn(cards, totals)
+  prompt MESSAGES[:dealer_turn]
+  loop do
+    display_hands(cards, totals)
+    sleep(SLEEP_DURATION)
+
+    if bust?(totals[:dealer])
+      break
+    elsif dealer_stay?(totals[:dealer])
+      prompt MESSAGES[:dealer_stay]
+      break
+    end
+    
+    deal_card!(:dealer, cards, totals)
+    prompt MESSAGES[:dealer_hit]
+  end
+end
+
+def deal_cards(cards, totals)
   2.times do
     deal_card!(:player, cards, totals)
     deal_card!(:dealer, cards, totals)
@@ -75,27 +146,46 @@ end
 
 def deal_card!(player, cards, totals)
   cards[player] = cards[:deck].pop
-  totals[player] = total(cards[player])
+  totals[player] = hand_total(cards[player])
 end
+
+# def hand_total(cards)
+#   sum = cards.reduce(0) { |sum, card| sum + value(card[1]) }
+  
+#   cards.count { |_, rank| rank == 'A' }
+#        .times { sum += 10 if sum + 10 <= WINNING_VALUE }
+#   sum
+# end
 
 def hand_total(cards)
-  sum = cards.reduce(0) { |sum, card| sum + value[card[1]] }
-  
-  if cards.count { |_, rank| rank == 'A' }
-    .times { sum += 10 if sum += 10 <= WINNING_VALUE }
-    sum
+  values = cards.map { |card| card[1] }
+  # binding.pry
+  sum = 0
+  values.each do |value|
+    if value == 'A'
+      sum += 1
+    elsif value.to_i == 0
+      sum += 10
+    else
+      sum += value.to_i
+    end
   end
+
+  cards.count { |_, rank| rank == 'A' }
+       .times { sum += 10 if sum + 10 <= WINNING_VALUE }
+
+  sum
 end
 
-def value(rank)
-  if NUMBERED_RANKS.include?(rank)
-    rank.to_i
-  elsif ROYAL_RANKS.include?(rank)
-    10
-  elsif rank == ACE
-    1
-  end
-end
+# def value(rank)
+#   if NUMBERED_RANKS.include?(rank)
+#     return rank.to_i
+#   elsif ROYAL_RANKS.include?(rank)
+#     return 10
+#   elsif rank == ACE
+#     return 1
+#   end
+# end
 
 # Display
 def display_welcome
@@ -114,9 +204,9 @@ end
 def display_hands(cards, totals, hide_dealer_card: false)
   clear_screen
   
-  prompt_pause(:title) 
-  prompt_pause(:players_hand)
-  display_cards(cards[:player, totals[:player]])
+  prompt(MESSAGES[:title], new_line = true) 
+  prompt MESSAGES[:players_hand]
+  display_cards(cards[:player], totals[:player])
   
   prompt_pause(:dealer_hand)
   if hide_dealer_card
@@ -128,7 +218,7 @@ end
 
 def display_cards(cards, total = nil)
   puts
-  puts cards_top(cards)
+  puts top_of_card(cards)
   puts middle_of_card(cards)
   puts bottom_of_card(cards)
   puts
@@ -157,15 +247,15 @@ end
 
 def display_totals(totals)
   sleep(SLEEP_DURATION)
-  puts MESSAGES(:separator_line)
+  puts MESSAGES[:separator_line]
   prompt "The dealer's hand's final total is #{totals[:dealer]}."
   prompt "Your hand's final total is #{totals[:player]}."
-  puts MESSAGES(:separator_line)
+  puts MESSAGES[:separator_line]
 end
 
 def evaluate_result(totals)
-  player_total = totals(:player)
-  dealer_total = totals(:dealer)
+  player_total = totals[:player]
+  dealer_total = totals[:dealer]
   if bust?(player_total)
     :player_busted
   elsif bust?(dealer_total)
@@ -183,50 +273,8 @@ def display_result(result)
   prompt_pause(result)
 end
 
-# Player Action
-def player_turn(cards, totals)
-  prompt_pause(:player_turn)
-  action = nil
-  loop do
-    action = get_player_action
-    deal_card!(:player, cards, totals) if action == :hit
-    display_state(cards, totals, hide_dealer_card: true)
-    break if action == :stay || bust?(totals[:player])
-  end
-end
-
-def get_player_action
-  answer = ''
-  loop do
-    prompt MESSAGES[:player_action]
-    answer = gets.chomp.downcase
-    break if %w(h hit s stay).include?(answer)
-    prompt MESSAGES[:invalid_input]
-  end
-
-  answer.start_with?('h') ? :hit : :stay
-end
-
-# Dealer Action
-def dealer_turn(cards, totals)
-  prompt_pause():dealer_turn)
-  loop do
-    display_state(cards, totals)
-    sleep(SLEEP_DURATION)
-
-    if bust?(totals[:dealer])
-      break
-    elsif dealer_stay?(totals[:dealer])
-      prompt MESSAGES[:dealer_stay]
-      break
-    end
-
-    deal_card!(:dealer, cards, totals)
-  end
-end
-
 # Auxillary methods
-def prompt_pause(action)
+def prompt_pause(action, new_line = false)
   puts ">> #{MESSAGES[action]}"
   sleep(1.5)
 end
@@ -271,25 +319,6 @@ def play_again?
   answer = valid_input
 
   answer.start_with?('y')
-end
-
-def play_twenty_one
-  display_welcome
-  loop do
-    play_match
-    break unless play_again?
-  end
-  display_goodbye
-end
-
-def play_match
-  cards = initialize_cards
-  totals = initialize_totals
-  deal_cards(cards, totals)
-  display_state(cards, totals, hide_dealer_card: true)
-  player_turn(cards, totals)
-  dealer_turn(cards, totals) unless bust?(totals[:player])
-  display_end_of_game(totals)
 end
 
 play_twenty_one
